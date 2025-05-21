c1b53e3e4476a8e1c47030e5a0689ca07c82fed7
perumallamanjula <perumalamanjula524@gmail.com>
2025-05-21 12:25:36 +0530

    Add files via upload

---
+++ Day1/p1.py
@@ -0,0 +1,8 @@
+## what is Azure Data Factory?
+
+Azure Data Factory (ADF) is a cloud-based data integration service designed to orchestrate and automate data workflows. 
+It is used to collect, transform, and deliver data, ensuring that insights are readily accessible for analytics and decision-making. 
+With its scalable and serverless architecture, ADF can handle workflows of any size—from simple data migrations to complex data transformation pipelines.
+ADF bridges the gap between data silos, enabling users to move and transform data between on-premises systems, cloud services, and external platforms. 
+
+
